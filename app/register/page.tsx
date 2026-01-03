390a40f19919e88aa50fad3dc555c902efe1a6db
jinuscript <jinuscript@gmail.com>
2026-01-03 14:56:12 +0900

    feat: 회원가입 페이지, 폼 UI, 서버 액션 추가

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/feature/register/ui/RegisterForm";
+
+export default function RegisterPage() {
+    return (
+        <main>
+            <h1>RegisterPage</h1>
+            <RegisterForm />
+        </main>
+    );
+}
