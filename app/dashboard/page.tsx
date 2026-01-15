95e9b0efbf915b3c83cc39415bd6b1b118f36a29
jinuscript <jinuscript@gmail.com>
2026-01-16 00:25:28 +0900

    chore: 사용하지 않는 코드 제거

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,4 @@
-import { apiClient } from "@/shared/api/apiClient";
-
 export default async function DashboardPage() {
-    const response = await apiClient('/purchase');
-    const data = await response.json();
-    console.log(data);
-
     return (
         <main>
             <h1>DashboardPage</h1>
