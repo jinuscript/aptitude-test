c9bd21b61974d64c2fb3cf70e782cc38f5c6b236
jinuscript <jinuscript@gmail.com>
2026-01-16 14:04:42 +0900

    feat: useInfiniteQuery를 사용해 무한스크롤 기능 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import OrderHistory from "@/feature/order-history/ui/OrderHistory";
+
 export default async function DashboardPage() {
     return (
         <main>
             <h1>DashboardPage</h1>
+            <OrderHistory />
         </main>
     );
 }
--- feature/order-history/model/orderHistoryQueryOptions.ts
+++ feature/order-history/model/orderHistoryQueryOptions.ts
@@ -1,18 +1,22 @@
 import { infiniteQueryOptions } from "@tanstack/react-query";
 
-export const getPurchaseHistoryQueryOptions = (
+export const getOrderHistoryQueryOptions = (
     fetcher: (url: string) => Promise<Response>
 ) => {
     return infiniteQueryOptions({
-        queryKey: ['order'],
+        queryKey: ['order-history'],
         queryFn: async ({ pageParam = 1 }) => {
-            const res = await fetcher(`?page=${pageParam}`);
-            if (!res.ok) {
-                throw new Error('Failed to fetch purchase history');
+            const response = await fetcher(`?page=${pageParam}`);
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message);
             }
-            return res.json();
+
+            return data;
         },
         initialPageParam: 1,
         getNextPageParam: (lastPage) => lastPage.nextPage ?? undefined,
+        retry: 1,
     });
 };
