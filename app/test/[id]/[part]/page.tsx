9d8483d562f47791883ec4adb1d1323e4eae5219
rlawlsdn263 <rlawlsdn263@naver.com>
2026-01-17 16:11:18 +0900

    feat: 테스트 문제 호출 기능 추가

--- app/test/[id]/[part]/page.tsx
+++ app/test/[id]/[part]/page.tsx
@@ -1,4 +1,12 @@
-const TestPartPage = () => {
+import { serverClient } from "@/shared/api/serverClient";
+
+const TestPartPage = async ({ params }: { params: Promise<{ part: string }> }) => {
+    const { part } = await params;
+
+    const response = await serverClient(`/test/${part}`);
+    const data = await response.json();
+    console.log(data)
+
     return <h1>TestPartPage</h1>
 }
 
