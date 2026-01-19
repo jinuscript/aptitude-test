0a9a259f68f9e6b9cd29c7f19ce609336cb88cdd
jinuscript <jinuscript@gmail.com>
2026-01-19 14:12:02 +0900

    feat: testAction에 API 응답에 따른 동적 라우팅 이동을 추가 (#10)

--- feature/submit-test/action/testAction.ts
+++ feature/submit-test/action/testAction.ts
@@ -1,13 +1,22 @@
 'use server';
 
+import { redirect } from "next/navigation";
 import { serverClient } from "@/shared/api/serverClient";
 
 export const testAction = async (formData: FormData) => {
     const rawData = Object.fromEntries(formData.entries());
-    const part = rawData.part
+    const part = rawData.part;
+    const test_id = rawData.test_id;
 
-    const response = await serverClient(`/test/${part}`);
-    const data = await response.json();
+    const response = await serverClient(`/test/${part}`, {
+        method: "POST",
+        body: JSON.stringify(rawData),
+    });
+    const { nextPart } = await response.json();
 
-    console.log(rawData);
+    if (nextPart === "finish") {
+        redirect(`/result/${test_id}`);
+    }
+
+    redirect(`/test/${test_id}/${nextPart}`);
 }
