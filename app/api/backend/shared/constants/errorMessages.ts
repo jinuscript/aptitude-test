0180032b171415c3b725b8132691ba3e9120dc28
jinuscript <jinuscript@gmail.com>
2026-01-21 14:00:47 +0900

    refactor(logic): 토큰이 유효하지 않을 때를 위한 서버 에러 메세지 추가

--- app/api/backend/shared/constants/errorMessages.ts
+++ app/api/backend/shared/constants/errorMessages.ts
@@ -4,6 +4,11 @@ export const ERROR_MESSAGES = {
         message: "유효하지 않은 입력값입니다.",
         details: []
     },
+    INVALID_TOKEN: {
+        code: "INVALID_TOKEN",
+        message: "유효하지 않은 토큰입니다.",
+        details: []
+    },
     AUTH_FAILED: {
         code: "AUTH_FAILED",
         message: "아이디 혹은 비밀번호를 확인해주세요.",
