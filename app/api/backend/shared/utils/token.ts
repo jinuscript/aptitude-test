a293a08addddd99aab202167e744379ae02290c5
jinuscript <jinuscript@gmail.com>
2026-01-22 12:32:08 +0900

    refactor(logic): accessToken 추출과 검증 로직을 별도의 유틸함수로 분리함

--- app/api/backend/shared/utils/token.ts
+++ app/api/backend/shared/utils/token.ts
@@ -1,11 +1,32 @@
-import { SignJWT } from 'jose';
+import { SignJWT, jwtVerify } from 'jose';
 import { JWT_SECRET } from '../constants/jwtSecret';
+import { ERROR_MESSAGES } from '../constants/errorMessages';
 
 interface UserPayload {
     user_id: string;
     role?: string;
     name?: string;
 }
+
+export const getAccessToken = (request: Request) => {
+    const authHeader = request.headers.get('Authorization');
+    const accessToken = authHeader?.substring(7);
+
+    if (!accessToken) {
+        throw new Error(ERROR_MESSAGES.INVALID_TOKEN.code);
+    }
+
+    return accessToken;
+};
+
+export const verifyAccessToken = async (accessToken: string) => {
+    try {
+        const { payload } = await jwtVerify(accessToken, JWT_SECRET);
+        return payload as unknown as UserPayload;
+    } catch (error) {
+        throw new Error(ERROR_MESSAGES.INVALID_TOKEN.code);
+    }
+};
 
 export const createAccessToken = (user: UserPayload) => {
     return new SignJWT({ user_id: user.user_id, role: user.role, name: user.name })
--- app/api/backend/test/route.ts
+++ app/api/backend/test/route.ts
@@ -2,21 +2,14 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from 'jose';
 
 import { readJsonDb } from '@/app/api/database/shared/utils/readJsonDb';
-import { ERROR_MESSAGES } from '@/app/api/backend/shared/constants/errorMessages';
-import { JWT_SECRET } from "@/app/api/backend/shared/constants/jwtSecret";
 
 export async function GET(request: Request) {
     try {
-        // 액세스 토큰 추출
-        const authHeader = request.headers.get('Authorization');
-        const token = authHeader?.substring(7);
-
-        if (!token) {
-            return NextResponse.json({ success: false, data: null, error: { code: ERROR_MESSAGES.INVALID_TOKEN.code, message: ERROR_MESSAGES.INVALID_TOKEN.message, details: [] } }, { status: 401 });
-        }
+        // 액세스 토큰 추출& 검증
+        const accessToken = getAccessToken(request);
 
         // 액세스 토큰 검증
-        const { payload } = await jwtVerify(token, JWT_SECRET) as { payload: { user_id: string } };
+        const payload = await verifyAccessToken(accessToken);
 
         // DB에서 사용자 테스트 정보 조회
         const allTests = await readJsonDb('app/api/database/data/test.json');
