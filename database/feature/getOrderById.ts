cbf154d8de9216a58f9a88eb903ea3bfd95032bc
jinuscript <jinuscript@gmail.com>
2026-01-28 13:59:18 +0900

    chore: 구 database 폴더를 제거함

--- database/feature/getOrderById.ts
+++
@@ -1,7 +0,0 @@
-import 'server-only';
-
-export const getOrderById = async (userOrderHistory: [], id: string) => {
-    const order = userOrderHistory.find(order => order.id === id);
-
-    return order;
-};
--- database/feature/getUserOrderHistory.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only';
-
-import { readJsonDb } from '../shared/readJsonDb';
-
-export const getUserOrderHistory = async (user_id: string) => {
-    const allOrderHistory = await readJsonDb("database/orderHistory.json");
-
-    return allOrderHistory[user_id] || [];
-};
