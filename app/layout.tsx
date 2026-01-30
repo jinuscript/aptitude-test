6421e8fef1a911fb38dc1e45a00f76a670b8a00f
jinuscript <jinuscript@gmail.com>
2026-01-30 15:32:21 +0900

    feat(ui): NextJS Local Font로 프리텐다드 폰트 추가 및 tailwind 변수 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import localFont from 'next/font/local'
 import "@/shared/styles/globals.css";
 import TanstackQueryProvider from "@/shared/provider/TanstackQueryProvider";
 
@@ -7,14 +8,21 @@ export const metadata: Metadata = {
   description: "AI 적성검사",
 };
 
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body>
+    <html lang="ko" className={pretendard.variable}>
+      <body className="font-pretendard">
         <TanstackQueryProvider>
           {children}
         </TanstackQueryProvider>
