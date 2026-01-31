029fc55e90c8e6dd6fedc3852a8284cb99684bcb
jinuscript <jinuscript@gmail.com>
2026-01-31 15:12:18 +0900

    refactor(ui): public용, private용 레이아웃을 만들어 네비게이션을 분리해서 관리

---
+++ app/(private)/layout.tsx
@@ -0,0 +1,5 @@
+const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
+    return <>{children}</>;
+};
+
+export default PrivateLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local'
 import "@/shared/styles/globals.css";
 import TanstackQueryProvider from "@/shared/provider/TanstackQueryProvider";
-import NavigationBar from "@/widget/NavigationBar/ui/NavigationBar";
 
 export const metadata: Metadata = {
   title: "AI 적성검사",
@@ -25,7 +24,6 @@ export default function RootLayout({
     <html lang="ko" className={pretendard.variable}>
       <body className="font-pretendard">
         <TanstackQueryProvider>
-          <NavigationBar />
           {children}
         </TanstackQueryProvider>
       </body>
