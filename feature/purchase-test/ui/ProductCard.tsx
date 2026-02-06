9be377b0cc06e6e58c6e10d2cae92b1cc06ffa9d
jinuscript <jinuscript@gmail.com>
2026-02-06 15:25:48 +0900

    feat(ui): 상품 목록, 상품 카드 컴포넌트 추가

--- feature/purchase-test/ui/ProductCard.tsx
+++ feature/purchase-test/ui/ProductCard.tsx
@@ -1,7 +1,12 @@
-const ProductCard = () => {
+import { type Product } from "../type/Product";
+import PurchaseButton from "./PurchaseButton";
+
+const ProductCard = ({ product }: { product: Product }) => {
     return (
         <div>
-            <h1>ProductCard</h1>
+            <h1>{product.name}</h1>
+            <p>{product.price}</p>
+            <PurchaseButton />
         </div>
     );
 };
--- feature/purchase-test/ui/ProductList.tsx
+++ feature/purchase-test/ui/ProductList.tsx
@@ -1,15 +1,18 @@
 import ProductCard from "./ProductCard";
 import { getProduct } from "../api/getProduct";
+import { type Product } from "../type/Product";
 
 const ProductList = async () => {
-    const products = await getProduct();
-
-    console.log(products);
+    const { data } = await getProduct();
 
     return (
         <div>
             <h1>ProductList</h1>
-            <ProductCard />
+            {
+                data.map((product: Product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))
+            }
         </div>
     );
 };
