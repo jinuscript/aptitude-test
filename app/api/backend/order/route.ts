c0fe0d20da8a3b14577a876cc4cc62ead4bf52b8
jinuscript <jinuscript@gmail.com>
2026-02-06 15:25:12 +0900

    feat(backend): 상품 구매를 하는 API 추가

--- app/api/backend/order/route.ts
+++ app/api/backend/order/route.ts
@@ -1,31 +1,64 @@
 import { NextResponse } from "next/server";
-import { jwtVerify } from 'jose';
 
-import { getUserOrderHistory } from "@/database/feature/getUserOrderHistory";
-import { ERROR_MESSAGES } from "@/shared/constants/errorMessages";
+import { ERROR_MESSAGES } from '@/app/api/backend/shared/constants/errorMessages';
+import { getAccessToken, verifyAccessToken } from "@/app/api/backend/shared/utils/token";
 
-const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+import { readJsonDb } from '@/app/api/database/shared/utils/readJsonDb';
+import { writeJsonDb } from '@/app/api/database/shared/utils/writeJsonDb';
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
+
+    const body = await request.json();
+    // const { code } = body;
+
     try {
-        // 1. 액세스 토큰 추출
-        const authHeader = request.headers.get('Authorization');
-        const token = authHeader?.substring(7);
+        // 액세스 토큰 추출& 검증
+        const accessToken = getAccessToken(request);
 
-        if (!token) {
-            return NextResponse.json({ message: ERROR_MESSAGES.AUTH_FAILED }, { status: 401 });
+        // 액세스 토큰 검증
+        const payload = await verifyAccessToken(accessToken);
+
+        const newTestSummary = {
+            testId: crypto.randomUUID(),
+            userId: payload.user_id,
+            code: "MID",
+            currentSection: null,
+            totalSection: 5,
+            purchaseDate: new Date().toISOString(),
+            status: "NOT_STARTED"
         }
 
-        // 2. 액세스 토큰 검증
-        const { payload } = await jwtVerify(token, JWT_SECRET) as { payload: { user_id: string } };
+        // DB에서 전체 테스트 요약 정보 조회
+        const testSummary = await readJsonDb('app/api/database/data/test-summary.json');
 
-        // 3. DB 로직
-        const userOrderHistory = await getUserOrderHistory(payload.user_id);
+        // 특정 사용자의 테스트 목록에 새 항목 추가
+        const userTestSummary = testSummary[payload.user_id] || [];
+        testSummary[payload.user_id] = [...userTestSummary, newTestSummary];
+
+        // 업데이트된 정보 저장
+        await writeJsonDb('app/api/database/data/test-summary.json', testSummary);
 
         return NextResponse.json({
-            userOrderHistory
+            success: true,
+            data: newTestSummary,
+            error: null
         }, { status: 200 });
+
     } catch (error) {
-        return NextResponse.json({ message: ERROR_MESSAGES.SERVER_ERROR }, { status: 500 });
+        if (error instanceof Error) {
+            if (error.message === "INVALID_TOKEN") {
+                return NextResponse.json({ success: false, data: null, error: { code: ERROR_MESSAGES.INVALID_TOKEN.code, message: ERROR_MESSAGES.INVALID_TOKEN.message, details: [] } }, { status: 401 });
+            }
+        }
+        return NextResponse.json({
+            success: false,
+            data: null,
+            error: {
+                code: ERROR_MESSAGES.SERVER_ERROR.code,
+                message: ERROR_MESSAGES.SERVER_ERROR.message,
+                details: []
+            }
+        },
+            { status: 500 });
     }
 }
