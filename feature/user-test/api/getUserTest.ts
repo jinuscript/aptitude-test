0d32f1b66bbd5785dd89a433d852e6c17e74a988
rlawlsdn263 <rlawlsdn263@naver.com>
2026-02-12 14:52:55 +0900

    chore: feature에 있던 test 파일 제거

--- feature/user-test/api/getUserTest.ts
+++
@@ -1,5 +0,0 @@
-import { serverClient } from "@/shared/api/serverClient";
-
-export const getUserTest = async () => {
-    return await serverClient('/test');
-};
--- feature/user-test/utils/createTestRoute.ts
+++
@@ -1,7 +0,0 @@
-export const createTestRoute = (status: string, code: string, testId: string, currentSection: string) => {
-    if (status === "COMPLETED") {
-        return `/result/${code}/${testId}`;
-    }
-
-    return `/test/${code}/${testId}/section/${currentSection}`;
-}
