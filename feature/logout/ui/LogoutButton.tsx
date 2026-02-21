cc340f4fea00e81e17f0570a9b4edb3adaa9dff0
jinuscript <jinuscript@gmail.com>
2026-02-22 00:42:03 +0900

    feat(ui): Logout 버튼에 스타일 추가

--- feature/logout/ui/LogoutButton.tsx
+++ feature/logout/ui/LogoutButton.tsx
@@ -6,7 +6,7 @@ const LogoutButton = () => {
     const { logout } = useLogout();
 
     return (
-        <button onClick={logout}>로그아웃</button>
+        <button onClick={logout} className="cursor-pointer bg-green-500 hover:bg-green-700 rounded-lg py-2 px-3 text-white w-full font-medium transition-colors">로그아웃</button>
     )
 }
 
