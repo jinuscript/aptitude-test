c8fa63ea885172940cacbd4b30c30422952c97af
jinuscript <jinuscript@gmail.com>
2026-02-22 00:43:44 +0900

    feat(ui): flex를 사용해 대시보드 페이지의 레이아웃 구축

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -3,12 +3,19 @@ import TestSummary from "@/widget/TestSummary/ui/TestSummary";
 
 export default async function DashboardPage() {
     return (
-        <main>
-            {/* 검사 요약 카드 */}
-            <TestSummary />
+        <main className="flex flex-col gap-8 min-h-screen">
+            {/* 페이지 제목 */}
+            <header>
+                <h1 className="text-2xl font-bold">대시보드</h1>
+            </header>
 
-            {/* 검사 리스트 */}
-            <TestList />
+            <div className="flex flex-col gap-4">
+                {/* 검사 요약 카드 */}
+                <TestSummary />
+
+                {/* 검사 리스트 */}
+                <TestList />
+            </div>
         </main>
     );
 }
