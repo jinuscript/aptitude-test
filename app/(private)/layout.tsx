ad21a52e863858ffa668507ae3337b77c2c6c9f3
jinuscript <jinuscript@gmail.com>
2026-02-22 00:44:11 +0900

    feat(ui): grid를 사용해 PrivateLayout의 레이아웃 구축

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -2,10 +2,12 @@ import PrivateNavigation from "@/widget/PrivateNavigation/ui/PrivateNavigation";
 
 const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>
+        <div className="grid grid-cols-[240px_1fr]">
             <PrivateNavigation />
-            {children}
-        </>
+            <div className="p-4 h-screen overflow-y-auto">
+                {children}
+            </div>
+        </div>
     );
 };
 
