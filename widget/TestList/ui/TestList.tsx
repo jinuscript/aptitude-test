8e0d993ea37419e8061250765b69c78e20f626ce
jinuscript <jinuscript@gmail.com>
2026-02-21 18:32:04 +0900

    feat(ui): TestList를 grid 레이아웃으로 변경

--- widget/TestList/ui/TestList.tsx
+++ widget/TestList/ui/TestList.tsx
@@ -8,7 +8,7 @@ const TestList = async () => {
     const { data } = await getTestList();
 
     return (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {data.map((test: Test) => (
                 <div key={test.testId}>
                     <TestCard test={test}>
